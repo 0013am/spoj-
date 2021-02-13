d8aea21e454ed9d5ca0bc91e848b7eb77afd7ec9
Ashish Sharma <65242615+0013am@users.noreply.github.com>
2021-02-13 22:26:27 +0530

    Update PT07Y.cpp

--- PT07Y.cpp
+++ PT07Y.cpp
@@ -3,51 +3,50 @@ using namespace std;
 vector<int>v[10005];
 int n;
 void fun(int src,bool*vis){
-              vis[src]=true;
-              for(auto nbr:v[src]){
-                            if(vis[nbr]==false){
-                                          fun(nbr,vis);
-              }
-             
+      vis[src]=true;
+      for(auto nbr:v[src]){
+         if(vis[nbr]==false){
+         fun(nbr,vis);
+          }       
     }
 }
 bool go(){
               bool*vis=new bool[n+1];
               for(int i=1;i<=n;i++){
-                            vis[i]=false;
+                    vis[i]=false;
               }
               int cnt=0;
               for(int i=1;i<=n;i++){
-                            if(vis[i]==false){
-                                          fun(i,vis);
-                                          cnt++;
-                            }
+                  if(vis[i]==false){
+                  fun(i,vis);
+                   cnt++;
+                 }
               }
               if(cnt==1){
-                            return true;
+                return true;
               }
-              return false;
+                return false;
 }
 int main() {
     int k;
     cin>>n>>k;
     for(int i=0;i<k;i++){
-                  int a,b;
-                  cin>>a>>b;
-                  if(a==b){
-                                cout<<"NO";
-                                return 0;
-                  }
-                  v[a].push_back(b);
-                  v[b].push_back(a);
+         int a,b;
+         cin>>a>>b;
+         if(a==b){
+         cout<<"NO";
+            return 0;
+         }
+         v[a].push_back(b);
+         v[b].push_back(a);
     }
    
     
-                  if(go() && k==n-1){
-                                cout<<"YES"<<endl;
-                  }else{
-                                cout<<"NO"<<endl;
-                  }
+     if(go() && k==n-1){
+         cout<<"YES"<<endl;
+      }else{
+        cout<<"NO"<<endl;
+      }
     
-	return 0;
+return 0;
 }
