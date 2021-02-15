ab5f5d85375b12e56880009a93a05ff9fb9122c3
Ashish Sharma <65242615+0013am@users.noreply.github.com>
2021-02-15 20:44:17 +0530

    Add files via upload

---
+++ PRATA.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool can(int *a,int n,int p,int mid){
+	int cnt=0;
+	for(int i=0;i<n;i++){
+        int c=0;
+		int pp=a[i];
+        int time=mid;
+		while(time>0){
+			time-=pp;
+            if(time>=0){
+                c+=1;
+                pp+=a[i];
+            }
+		}
+        cnt+=c;
+        if(cnt>=p){
+            return true;
+        }
+	}
+	return  false;
+}
+int go(int *a,int n,int p){
+	int l=0,r=10000007,ans=-1;
+	while(l<=r){
+		int mid=(l+r)/2;
+		if(can(a,n,p,mid)){
+			ans=mid;
+			r=mid-1;
+		}else{
+			l=mid+1;
+		}
+	}
+	return ans;
+}
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int p; cin>>p;
+		int n; cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		cout<<go(a,n,p)<<endl;
+	}
+} 
